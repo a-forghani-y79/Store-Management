59de9bff5cbdd7609916a4ac46312ae858926a44
ali-forghani <a.forghani.y79@gmail.com>
2025-03-05 03:35:24 +0330

    add validation of BasePaginationRequestDTO

--- src/main/java/com/eight/hundred/group/store/dto/request/base/BasePaginationRequestDTO.java
+++ src/main/java/com/eight/hundred/group/store/dto/request/base/BasePaginationRequestDTO.java
@@ -1,5 +1,7 @@
 package com.eight.hundred.group.store.dto.request.base;
 
+import jakarta.validation.constraints.Positive;
+import jakarta.validation.constraints.PositiveOrZero;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
@@ -10,6 +12,8 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @EqualsAndHashCode(callSuper = true)
 public class BasePaginationRequestDTO extends BaseRequestDTO {
+    @PositiveOrZero(message = "pagination.request.page.positive-or-zero")
     private Integer page;
+    @Positive(message = "pagination.request.size.positive")
     private Integer size;
 }
